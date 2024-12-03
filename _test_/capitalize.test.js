fc072807d2e9e446952b7fe57050bffed4ae9c19
Pavel Sofronov <sofronovpv89@mail.ru>
2024-12-03 13:03:07 +0400

    install jest

---
+++ _test_/capitalize.test.js
@@ -0,0 +1,8 @@
+import { capitalize } from '../src/capitalize.js';
+import { strict as assert } from 'node:assert';
+
+assert.deepEqual(capitalize('hello'),'Hello');
+
+assert.deepEqual(capitalize(''), '');
+
+console.log('Все тесты пройдены!');
